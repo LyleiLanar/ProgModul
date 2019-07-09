378e1759b9e8513968725f314f9e1b636457d629
LyleiLanar <49948092+LyleiLanar@users.noreply.github.com>
2019-07-09 15:27:04 +0200

    hazi feladat pushh

---
+++ HomeWork/HomeWork_II/AdottSzamOsszesOsztoja/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AdottSzamOsszesOsztoja
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Kérlek add meg azt a számot, aminek szeretnénk megismerni az összes osztóját: ");
+            int szam = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine();
+            Console.Write($"{szam} összes osztója: ");
+            for (int i = 1; i < szam; i++)
+            {
+                if (szam % i == 0)
+                {
+                    Console.Write($"{i}, ");
+                }
+            }
+            Console.Write($"{szam}.");
+            Console.ReadLine();
+        }
+    }
+}
---
+++ HomeWork/HomeWork_II/FejVagyIras/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FejVagyIras
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Hányszor dobjunk fel egy érmét: ");
+            int feldobasokSzama = Convert.ToInt32(Console.ReadLine());
+            int fejekSzama = 0;
+
+            Random rnd = new Random();
+
+            for (int i = 0; i < feldobasokSzama; i++)
+            {
+                //  nulla a fej, egy az írás
+                if (rnd.Next(0, 2) == 0)
+                {
+                    fejekSzama++;
+                }
+            }
+
+            Console.WriteLine($"Összesen {fejekSzama} FEJ!");
+            Console.WriteLine($"Összesen {feldobasokSzama - fejekSzama} ÍRÁS!");
+
+        }
+    }
+}
