1b0991d90f5383cdcdd85652c55df7e3497b25bb
LyleiLanar <49948092+LyleiLanar@users.noreply.github.com>
2019-07-24 16:03:29 +0200

    házi feladat

--- 0722/OraiMunka/KedvencFogyasztas/Form1.cs
+++ 0722/OraiMunka/KedvencFogyasztas/Form1.cs
@@ -1,11 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows.Forms;
 
 namespace KedvencFogyasztas
@@ -21,9 +14,12 @@ namespace KedvencFogyasztas
         {
             string szöveg = txtTetel.Text;
 
-            if (szöveg != "")
-            {                
-                    lstTetelek.Items.Add(txtTetel.Text);                                                          
+            if (Int32.TryParse(szöveg, out int szam) == true)
+            {
+                if (szöveg != "")
+                {
+                    lstTetelek.Items.Add(txtTetel.Text);
+                }
             }
 
             txtTetel.Text = "";
@@ -32,15 +28,145 @@ namespace KedvencFogyasztas
 
         private void BtnMasol_Click(object sender, EventArgs e)
         {
-            if (txtTetel.Text != "")
+            for (int i = 0; i < lstTetelek.SelectedItems.Count; i++)
             {
-                
+                if (txtEredmeny.Text == "")
+                {
+                    txtEredmeny.Text = txtEredmeny.Text + (string)lstTetelek.SelectedItems[i] + "\r\n";
+                }
+                else
+                {
+                    txtEredmeny.Text = txtEredmeny.Text + (string)lstTetelek.SelectedItems[i] + "\r\n";
+                }
             }
+
+            lstTetelek.SelectedItems.Clear();
         }
 
         private void TxtTetel_TextChanged(object sender, EventArgs e)
         {
 
+        }
+
+        private void BtnMaximum_Click(object sender, EventArgs e)
+        {
+            string tetelek = txtEredmeny.Text;
+            bool vanAdat;
+            int adat;
+            int max = Int32.MinValue;
+            int min = Int32.MaxValue;
+
+            if (tetelek != "")
+            {
+                vanAdat = Int32.TryParse(tetelek.Substring(0, tetelek.IndexOf("\r\n")), out adat);
+            }
+            else
+            {
+                vanAdat = false;
+                adat = 0;
+            }
+
+
+            while (vanAdat)
+            {
+                if (max < adat) max = adat;
+                if (min > adat) min = adat;
+
+                tetelek = tetelek.Remove(0, adat.ToString().Length + 2);
+
+                if (tetelek != "")
+                {
+                    adat = Convert.ToInt32(tetelek.Substring(0, tetelek.IndexOf("\r\n")));
+                }
+                else
+                {
+                    vanAdat = false;
+                }
+            }
+            txtMaxEredmeny.Text = max.ToString() + "/" + min.ToString();
+        }
+
+        private void BtnSzum_Click(object sender, EventArgs e)
+        {
+            string tetelek = txtEredmeny.Text;
+            bool vanAdat;
+            int adat;
+            int szum = 0;
+
+            if (tetelek != "")
+            {
+                vanAdat = Int32.TryParse(tetelek.Substring(0, tetelek.IndexOf("\r\n")), out adat);
+            }
+            else
+            {
+                vanAdat = false;
+                adat = 0;
+            }
+
+            while (vanAdat)
+            {
+                szum += adat;
+
+                tetelek = tetelek.Remove(0, adat.ToString().Length + 2);
+
+                if (tetelek != "")
+                {
+                    adat = Convert.ToInt32(tetelek.Substring(0, tetelek.IndexOf("\r\n")));
+                }
+                else
+                {
+                    vanAdat = false;
+                }
+            }
+            txtSzumEredmeny.Text = szum.ToString();
+        }
+
+        private void BtnEredmenyTorles_Click(object sender, EventArgs e)
+        {
+            txtEredmeny.Text = "";
+            txtMaxEredmeny.Text = "";
+            txtSzumEredmeny.Text = "";
+            txtParosEredmeny.Text = "";
+        }
+
+        private void BtmParosSzamok_Click(object sender, EventArgs e)
+        {
+            string tetelek = txtEredmeny.Text;
+            bool vanAdat;
+            int adat;
+            string parosSzamok = "";
+
+            if (tetelek != "")
+            {
+                vanAdat = Int32.TryParse(tetelek.Substring(0, tetelek.IndexOf("\r\n")), out adat);
+            }
+            else
+            {
+                vanAdat = false;
+                adat = 0;
+            }
+
+            while (vanAdat)
+            {
+                if (adat % 2 == 0) parosSzamok = parosSzamok + " " +adat.ToString();
+
+                tetelek = tetelek.Remove(0, adat.ToString().Length + 2);
+
+                if (tetelek != "")
+                {                    
+                    adat = Convert.ToInt32(tetelek.Substring(0, tetelek.IndexOf("\r\n")));                   
+                }
+                else
+                {
+                    vanAdat = false;
+                }
+            }
+            txtParosEredmeny.Text = parosSzamok;
+        }
+
+        private void BtnTorles_Click(object sender, EventArgs e)
+        {
+            lstTetelek.Items.Clear();
         }
     }
 }
