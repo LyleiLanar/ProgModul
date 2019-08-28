4b167e48beccbb155f092a781e0503fdb59619e1
LyleiLanar <49948092+LyleiLanar@users.noreply.github.com>
2019-08-28 20:01:51 +0200

    órai munka

---
+++ 0828/ÓraiMunka/MasodpercAtalakito/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace MasodpercAtalakito
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // egy másodperc értékről váltson vissza óra:perc:másodperc értékre.
+
+            Console.WriteLine(TimeFromSec(1250000));
+            Console.WriteLine(TimeToSec(1,1,1));
+        }
+
+        static int TimeToSec(int hours, int minutes, int secunds)
+        {
+            return hours * 3600 + minutes * 60 + secunds;
+        }
+
+        static string TimeFromSec(int secunds)
+        {
+            int minutes = secunds / 60;
+            secunds %= 60;
+
+            int hours = minutes / 60;
+            minutes %= 60;
+
+            int days = hours / 24;
+            hours %= 24;
+
+            return $"{secunds:D2} nap {hours:D2}:{minutes:D2}:{secunds:D2}";
+        }
+
+
+    }
+}
---
+++ 0828/ÓraiMunka/NyugtaModern/Program.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NyugtaModern
+{
+    class Program
+    {
+        static int HOSSZ;
+        static void Main(string[] args)
+        {
+            string[] tetelek = { "kutya", "macska", "görény", "medvedisznó-ember" };
+            int[] arak = { 2000, 2040, 2500, 5400 };
+            
+            for (int i = 0; i < tetelek.Length; i++)
+            {
+                int aktHossz = tetelek[i].Length + arak[i].ToString().Length+6;
+                if (aktHossz>HOSSZ)
+                {
+                    HOSSZ = aktHossz;
+                }                
+            }
+            Console.WriteLine(HOSSZ);
+            Vonal(HOSSZ, '*');
+            SzovegTeljesenKozepre("NYUGTA");
+            SzovegJobbraKozepre("Aláírás");
+            
+
+            Vonal(HOSSZ,'*');
+            Vonal(HOSSZ / 2, '_', false);
+        }
+
+        private static void Vonal(int hossz, char jel,bool balra = false)
+        {
+            if (!balra)
+            {
+                for (int i = 0; i < HOSSZ - hossz; i++)
+                {
+                    Console.Write(" ");
+                }
+
+                for (int i = 0; i < hossz; i++)
+                {
+                    Console.Write(jel);
+                }
+            }
+            Console.WriteLine();
+            
+        }
+
+        private static void SzovegTeljesenKozepre(string szoveg)
+        {
+            int spaceSzamok = HOSSZ / 2 - szoveg.Length / 2;
+
+            for (int i = 0; i < spaceSzamok; i++)
+            {
+                Console.Write(" ");
+            }
+            Console.WriteLine(szoveg);
+        }
+
+        private static void SzovegJobbraKozepre(string szoveg)
+        {
+            int spaceSzamok = HOSSZ/4 - szoveg.Length / 2;
+            
+            for (int i = 0; i < spaceSzamok; i++)
+            {
+                szoveg += " ";
+            }
+            Console.WriteLine($"{szoveg,-0}");
+        }
+    }
+}
